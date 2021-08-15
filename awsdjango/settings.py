50c8433c592c49ad92ac8dbfbe4dee611e26618b
Ray-Abel <r.abel-obeya@semicolon.africa>
2021-08-15 09:35:28 +0100

    ...ll

--- awsdjango/settings.py
+++ awsdjango/settings.py
@@ -17,12 +17,6 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# reading .env file
-environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
