91fc266bd034df5c7854c9f320750eed78d2aa82
Ray-Abel <r.abel-obeya@semicolon.africa>
2021-08-15 09:39:16 +0100

    ...ll

--- awsdjango/settings.py
+++ awsdjango/settings.py
@@ -21,7 +21,7 @@ import environ
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = '12223jjgnnmffffjfjjf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
