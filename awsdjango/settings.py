1ced7f366fade8b79d5dfd1aaccbcf762afbfe4f
Ray-Abel <r.abel-obeya@semicolon.africa>
2021-08-15 08:09:04 +0100

    ...j

--- awsdjango/settings.py
+++ awsdjango/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'awsdjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': "django.db.backends.postgresql",
-        'HOST': env("$POSTGRES_HOST"),
+        'HOST': env("POSTGRES_HOST"),
         'PORT': 5432,
-        'NAME': env("$POSTGRES_DB"),
-        'USER': env("$POSTGRES_USER"),
-        'PASSWORD': env("$POSTGRES_PASSWORD"),
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
 
     }
 }
